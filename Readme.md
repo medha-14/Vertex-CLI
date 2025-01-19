8bb038d5f0ab7a00e8c722b83c10d32f254ebfea
Prtm2110 <prathamhole@gmail.com>
2025-01-19 16:05:08 +0530

    created cli dir and added support for json file for api keys

---
+++ Readme.md
@@ -0,0 +1,1 @@
+`python3 Vertex-CLI/cli/prompt.py ".........your prompt......" --confing <gemini_model> <api key>`
--- cli/ai_models.py
+++ cli/ai_models.py
@@ -3,13 +3,15 @@ import google.generativeai as genai
 
 FILE_NAME = "models_api.json"
 
+
 def load_models_api():
     try:
         with open(FILE_NAME, 'r') as f:
             return json.load(f)
     except FileNotFoundError:
-        return {}
-
+        print("File not found")    
+                
+        
 def update_models_api_json(updated_json_data):
     with open(FILE_NAME, 'w') as f:
         json.dump(updated_json_data, f, indent=4)
@@ -37,8 +39,10 @@ def configure_model(model_name, api_key):
     update_models_api_json(models_api_dict)
     print("Model added successfully.")
 
+
 def generate_output(model_name, prompt_by_user):
-    api_key = api_key_model_selection(model_name)
+    # api_key = api_key_model_selection(model_name)
+    api_key = "AIzaSyCWKme9gf6v_9aqQWCC7tUyYZxoqXVHIfQ"
     genai.configure(api_key=api_key)
     model = genai.GenerativeModel(model_name)
     response = model.generate_content(prompt_by_user)
