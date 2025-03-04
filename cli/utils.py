cc836df37cf63c27d25d403b5640b849adad3038
Prtm2110 <prathamhole@gmail.com>
2025-03-04 10:19:31 +0530

    readme and minor

--- cli/utils.py
+++ cli/utils.py
@@ -18,30 +18,3 @@ def spin_loader(stop_event):
         sys.stdout.write("\b")
     sys.stdout.write(" ")
     sys.stdout.flush()
-
-
-def install_requirements(requirements_file="requirements.txt"):
-    """
-    Installs dependencies from a given requirements.txt file.
-    Works across different OS environments.
-    """
-    import subprocess
-    import sys
-    import os
-
-    if not os.path.exists(requirements_file):
-        print(f"Error: {requirements_file} not found!")
-        return
-
-    try:
-        subprocess.run(
-            [sys.executable, "-m", "pip", "install", "-r", requirements_file],
-            check=True,
-        )
-        print(f"Successfully installed dependencies from {requirements_file}.")
-
-        from cli.ai_models import create_json_file
-
-        create_json_file()
-    except subprocess.CalledProcessError as e:
-        print(f"Installation failed: {e}")
